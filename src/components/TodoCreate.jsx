1d93a0aca04b926e045c56d96492c5176691c066
Pawan Kumar <108563158+pksingh1508@users.noreply.github.com>
2023-07-09 17:38:59 +0530

    adding base url

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -10,7 +10,7 @@ const TodoCreate = () => {
 
     const submit = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://localhost:4000/api/v1/createTodo', {
+        const response = await fetch(`${process.env.BASE_URL}/createTodo`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
